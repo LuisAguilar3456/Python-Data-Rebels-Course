0c8feab62d922d3d9cd5a70433eb7cdf6a624c15
LuisAguilar3456 <LuisE.AguilarMoreno@outlook.com>
2023-05-15 12:51:35 -0600

    Actividad 4

---
+++ Actividad4.py
@@ -0,0 +1,12 @@
+x,y=24,6
+x//=2
+y+=15
+y*=3
+print("Yo tengo",x,"de edad")
+print("Mi hermana tiene",y,"de edad")
+if x>y:
+    print("Yo soy mas grande que mi hermana, yo tengo",x,"años y ella",y,"años")
+elif y>x:
+    print("Mi hermana es mams grande que yo, ella tiene",y,"años y yo",x,"años de edad")
+else:
+    print("Mi hermana y yo tenemos la misma edad")
